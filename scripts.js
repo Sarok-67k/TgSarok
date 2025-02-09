db650963b25e28469bf13a9735447c2dde5a3931
Uthman <116843681+Sarok-67k@users.noreply.github.com>
2025-02-09 14:36:45 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function toggleContactForm() {
+    var contactForm = document.getElementById('contactForm');
+    contactForm.style.display = (contactForm.style.display === 'flex') ? 'none' : 'flex';
+}
